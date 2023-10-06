9fff3c27d950061b5f0a6fb031c7d645c2793f56
Nayan Verma <nayan.v9560@gmail.com>
2023-10-06 22:34:34 +0530

    Deleted Previous App and Created New
    In previous one material design edit text was having a lot of issue bcz of some settings and i guess animations
    
    Created a fresh app will be doing animations at the end
    Successfully added text input layouts

---
+++ Android App/FindTheFake/app/src/main/java/com/example/findthefake/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.findthefake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signup = findViewById(R.id.already);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Android App/FindTheFake/app/src/main/java/com/example/findthefake/SignUp.java
@@ -0,0 +1,30 @@
+package com.example.findthefake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    Button login;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        login = findViewById(R.id.already);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
